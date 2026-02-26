62a51b6d08d69aee517798ae0e6620788fc98b65
vkfro2293 <vkfro2293@naver.com>
2026-02-26 18:56:46 +0900

    프로그래머스: 롤케이크

---
+++ PRG/PRG_롤케이크.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int t : topping) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+
+        for(int i=0; i<topping.length; i++) {
+            set.add(topping[i]);
+            map.put(topping[i], map.getOrDefault(topping[i], 0) - 1);
+            if(map.get(topping[i]) == 0) {
+                map.remove(topping[i]);
+            }
+
+            if(set.size() == map.size()) answer++;
+        }
+
+
+        return answer;
+    }
+}

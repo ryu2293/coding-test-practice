3cf128e0c40d8271d32350c4167e51e34c6541eb
vkfro2293 <vkfro2293@naver.com>
2026-03-21 15:59:22 +0900

    백준: 11403 경로 찾기

---
+++ BOJ/BOJ_11403.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][n];
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int k=0; k<n; k++) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(arr[i][k] == 1 && arr[k][j] == 1) {
+                        arr[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                answer.append(arr[i][j] + " ");
+            }
+            answer.append("\n");
+        }
+        System.out.println(answer);
+    }
+
+}
